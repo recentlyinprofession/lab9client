634aab0659f78c43962343d94f5520c3bec2789d
Anastasia <56310790+recentlyinprofession@users.noreply.github.com>
2020-05-08 14:52:17 +0300

    pred_deploy

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ const cache = new InMemoryCache();
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: 'https://apolloqllabnew.herokuapp.com',
     headers: {
       authorization: localStorage.getItem('token'),
       'client-name': 'Space Explorer [web]',
